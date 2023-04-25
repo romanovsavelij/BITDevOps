7ae35d0b98cec260a13b9268fa9d20771da75119
romanovsavelij <romanovsavelij@mail.ru>
2023-04-25 19:12:33 +0300

    run script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Building Docker images..."
+docker build -t api-gateway -f api-gateway/Dockerfile .
+docker build -t api-wrapper -f api-wrapper/Dockerfile .
+
+echo "Applying Kubernetes manifests and Istio configurations..."
+kubectl apply -f api-gateway/deployment.yaml
+kubectl apply -f api-wrapper/deployment.yaml
+kubectl apply -f ingress.yaml
+kubectl apply -f egress.yaml
