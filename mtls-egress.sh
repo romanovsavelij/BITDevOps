e312c571042999b3e026001504a982ac815cd657
romanovsavelij <romanovsavelij@mail.ru>
2023-05-23 19:15:33 +0300

    add readme for ht3

--- mtls-egress.sh
+++ mtls-egress.sh
@@ -1,14 +1,3 @@
-kubectl create namespace mesh-external
+#!/bin/bash
 
-kubectl create -n mesh-external secret tls nginx-server-certs --key my-nginx.mesh-external.svc.cluster.local.key --cert my-nginx.mesh-external.svc.cluster.local.crt
-kubectl create -n mesh-external secret generic nginx-ca-certs --from-file=example.com.crt
-
-kubectl create configmap nginx-configmap -n mesh-external --from-file=nginx.conf=./nginx.conf
-
-kubectl apply -f example-egress/nginx.yaml
-
-kubectl create secret -n istio-system generic client-credential --from-file=tls.key=client.example.com.key \
-  --from-file=tls.crt=client.example.com.crt --from-file=ca.crt=example.com.crt
-
-kubectl apply -f example-egress/egress.yaml
-kubectl apply -n istio-system -f example-egress/origination.yaml
+kubectl exec "$(kubectl get pod -l app=sleep -o jsonpath={.items..metadata.name})" -c sleep -- curl -sS http://my-nginx.mesh-external.svc.cluster.local
--- start.sh
+++ start.sh
@@ -11,3 +11,19 @@ kubectl apply -f ingress.yaml
 kubectl apply -f egress.yaml
 kubectl apply -f example/ingress.yaml
 kubectl apply -f example/httpbin.yaml
+
+echo "Third task addons"
+kubectl create namespace mesh-external
+
+kubectl create -n mesh-external secret tls nginx-server-certs --key my-nginx.mesh-external.svc.cluster.local.key --cert my-nginx.mesh-external.svc.cluster.local.crt
+kubectl create -n mesh-external secret generic nginx-ca-certs --from-file=example.com.crt
+
+kubectl create configmap nginx-configmap -n mesh-external --from-file=nginx.conf=./nginx.conf
+
+kubectl apply -f example-egress/nginx.yaml
+
+kubectl create secret -n istio-system generic client-credential --from-file=tls.key=client.example.com.key \
+  --from-file=tls.crt=client.example.com.crt --from-file=ca.crt=example.com.crt
+
+kubectl apply -f example-egress/egress.yaml
+kubectl apply -n istio-system -f example-egress/origination.yaml
